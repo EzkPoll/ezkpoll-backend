cc3f082b26390e1a820bb1b9853e00a127c660e8
abner <fantasrulai@gmail.com>
2024-03-24 01:17:48 +0800

    update genMaciKeyPair

--- src/maci/maci.controller.ts
+++ src/maci/maci.controller.ts
@@ -1,11 +1,16 @@
 import { CacheTTL } from '@nestjs/cache-manager';
 import { MaciService } from './maci.service';
-import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiProperty, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { PrismaService } from 'src/prisma.service';
-import { randomBytes } from 'ethers';
+import { keccak256, randomBytes } from 'ethers';
 import { uint8ArrayToBn } from 'utils';
 import { Logger } from '@nestjs/common';
+
+class GenMaciKeyPairRequest {
+  @ApiProperty()
+  seed: string;
+}
 
 @ApiTags('maci')
 @Controller('maci')
@@ -23,10 +28,13 @@ export class MaciController {
   }
 
   @Get('gen')
-  genMaciKeyPair() {
-    // const random = uint8ArrayToBn(randomBytes(32));
-    // Logger.log('random', random.toString());
-    const result = this.maciService.genMaciKeyPair(undefined);
+  @ApiResponse({
+    status: 200,
+    description: 'Generate MACI key pair',
+  })
+  genMaciKeyPair(@Query() dto: GenMaciKeyPairRequest) {
+    const hash = keccak256(dto.seed);
+    const result = this.maciService.genMaciKeyPair(BigInt(hash));
     Logger.log({
       genMaciKeyPair: result,
     });
